ab1001fe082a7716b133fdec1727584a886781ea
Puja Barnwal <pujabarnwal3@gmail.com>
2022-11-27 02:00:22 +0530

    product list component and enviroment setup and also ag-grid

--- src/app/crud/product-list/product-list.component.ts
+++ src/app/crud/product-list/product-list.component.ts
@@ -1,27 +1,105 @@
 import { Component, OnInit } from '@angular/core';
-import { CRUDService } from '../services/crud.service';
+import {CRUDService} from "../services/crud.service";
+import {Router} from "@angular/router";
+
+
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-    columnDefs = [
-          { field: 'make' },
-          { field: 'model' },
-          { field: 'price' }
-      ];
+  columnDefs = [
+    {
+      field: 'p_name',
+      headerName: 'Name',
+      sortable: true,
+      headerClass: 'header-cell'
+    },
+    {
+      field: 'p_description',
+      headerName: 'Desc',
+      sortable: true,
+      headerClass: 'header-cell'
+    },
+    {
+      field: 'p_price',
+      headerName: 'Price',
+      sortable: true,
+      headerClass: 'header-cell'
+    },
+    {
+      field: '',
+      headerName: 'Actions',
+      headerClass: 'header-cell',
+      width: 250,
+      cellRenderer: this.actionRender.bind(this)
+    }
+  ];
+  rowData: any = [];
+  gridOptions = {
+    rowHeight: 50
+  }
 
-      rowData = [
-          { make: 'Toyota', model: 'Celica', price: 35000 },
-          { make: 'Ford', model: 'Mondeo', price: 32000 },
-          { make: 'Porsche', model: 'Boxster', price: 72000 }
-      ];
-
-  constructor(private crudService : CRUDService) { }
+  productList: any = [];
+  productListSubscribe: any;
+  constructor(private crudService: CRUDService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.getProductList();
   }
+
+  getProductList() {
+    this.productListSubscribe = this.crudService.loadProducts().subscribe(res => {
+      this.productList = res;
+      this.rowData = res;
+    })
+  }
+
+  actionRender(params: any) {
+    let div = document.createElement('div');
+    let htmlCode = '<button type="button" class="btn btn-success">View</button>\n' +
+      '<button type="button" class="btn btn-danger">Delete</button>\n' +
+      '<button type="button" class="btn btn-warning">Edit</button>'
+    div.innerHTML = htmlCode;
+    //handle view button
+    let viewButton = div.querySelector('.btn-success');
+    // @ts-ignore
+    viewButton.addEventListener('click', () => {
+      this.viewProductDetails(params);
+    });
+
+    //handle edit button
+    let editButton = div.querySelector('.btn-warning');
+    // @ts-ignore
+    editButton.addEventListener('click', () => {
+      this.editProductDetails(params);
+    });
+
+    //handle delete button
+    let deleteButton = div.querySelector('.btn-danger');
+    // @ts-ignore
+    deleteButton.addEventListener('click', () => {
+      this.deleteProduct(params);
+    });
+    return div;
+  }
+
+  viewProductDetails(params: any) {
+    this.router.navigate(['/crud/view-product-details/' + params.data.p_id]);
+  }
+
+  editProductDetails(params: any) {
+    this.router.navigate(['/crud/update-product/' + params.data.p_id]);
+  }
+
+  deleteProduct(params: any) {
+
+  }
+
+
 
 }
